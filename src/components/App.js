845aeedcca20d1f2aa9fb25eeb9032ef9b5474f3
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-02 13:50:14 -0400

    update css, routing, etc

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,29 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
 import Login from './Login';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 export default function  App() {
   return (
-    <div id="root" className="App">
-      <Login></Login>
-    </div>
+    <Navbar bg="dark" variant="dark" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="./Login">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Navbar.Text>
+      Signed in as: <a href="#login">Mark Otto</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Navbar>
   );
 }
