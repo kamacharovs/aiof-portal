88a20891aff9053c9a6234bf4367b6a1b5de83e5
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-02 09:25:02 -0400

    update

--- aiof-portal/src/components/Navbar.js
+++ aiof-portal/src/components/Navbar.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-class Navbar extends Component
-{
-    render() {
-        return (
-            <div>
-              <ul id="nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </div>
-        );
-    }
+function PortalNavbar() {
+//const PortalNavbar = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div id="portal-navbar" className="PortalNavbar">
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default PortalNavbar;
--- aiof-portal/src/index.js
+++ aiof-portal/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import PortalNavbar from './components/Navbar';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//ReactDOM.render(<PortalNavbar />, document.getElementById('portal-navbar'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
