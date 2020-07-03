3cf22c2c3882172ea217b46d4cb6da805e97eb6a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-03 07:39:57 -0400

    add sign up page

---
+++ src/components/SignUp.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import './SignUp.css';
+import { LinkContainer } from 'react-router-bootstrap';
+import Login from './Login';
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <>
+                <div class="login d-flex justify-content-center">
+                    <h3>Sign Up</h3>
+                    <Form>
+                        <Form.Group controlId="formFirstName">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control type="text" placeholder="First name" />
+                            <Form.Text className="text-muted">
+                                Legal first name
+                        </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formLastName">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control type="text" placeholder="Last name" />
+                            <Form.Text className="text-muted">
+                                Legal last name
+                        </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                        </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                    </Button>
+                        <p className="forgot-password text-right">
+                            Already registered <LinkContainer to="/login"><a href="#">sign in?</a></LinkContainer>
+                        </p>
+                    </Form>
+                </div>
+
+                <Route path='/login' component={Login} />
+            </>
+        );
+    }
+}
