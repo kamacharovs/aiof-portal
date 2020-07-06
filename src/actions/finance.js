dae5746c96924587787e8a9be7eb36d28e06e5ae
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-06 13:21:23 -0400

    add base actions/reducers/store

---
+++ src/actions/finance.js
@@ -0,0 +1,7 @@
+export const AIOF_BASE_URL = 'http://localhost:5000/aiof/'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+export async function getFinanceAsync(id) {
+  return await fetch(AIOF_BASE_URL + "finance/" + id)
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import aiofApp from './reducers/aiof';
+
+const store = createStore(aiofApp)
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
