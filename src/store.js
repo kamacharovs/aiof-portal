4d5fa6abe05f45d46185b8cbb64a1a0c8486f8e7
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-07 07:30:46 -0400

    update to not call dispatch(getuser)

--- src/store.js
+++ src/store.js
@@ -1,19 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
+import thunkMiddleware  from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import rootReducer from './reducers';
-import { getUser } from './actions/user';
 
 const loggerMiddleware = createLogger()
 
 const store = createStore(
     rootReducer,
     applyMiddleware(
-        thunk, 
+        thunkMiddleware, 
         loggerMiddleware
     )
 );
-
-store.dispatch(getUser('gkama')).then(() => console.log(store.getState()))
 
 export default store
