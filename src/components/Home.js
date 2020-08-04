4b076317f168c340fd1a27b14ff4b3482b1ccf66
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-04 13:01:43 -0400

    update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,36 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Jumbotron, Container } from 'react-bootstrap';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      user : {}
+      user: {}
     }
   }
 
   render() {
     return (
-      <div>
-        <h1>This is home</h1>
-      </div>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+    </p>
+        </Container>
+      </Jumbotron>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-      username: state.user.username,
-      user: state.user.user,
-      isLoggedIn: state.user.isLoggedIn
+    username: state.user.username,
+    user: state.user.user,
+    isLoggedIn: state.user.isLoggedIn
   };
 };
 
