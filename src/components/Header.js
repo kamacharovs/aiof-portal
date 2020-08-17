069ec0e3a7f751cbb1c50da7096f6ad022f3bae1
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-17 14:30:17 -0400

    add react-bootstrap and update menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,20 @@
 import React from 'react';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const LoggedOutView = props => {
   if (!props.currentUser) {
     return (
-      <ul className="nav navbar-nav pull-xs-right">
-
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            Sign in
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/register" className="nav-link">
-            Sign up
-          </Link>
-        </li>
-
-      </ul>
+      <Navbar bg="light" variant="light" expand="lg" sticky="top">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/login" className="nav-link">Sign in</Link>
+            <Link to="/register" className="nav-link">Sign up</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
   return null;
@@ -33,35 +23,24 @@ const LoggedOutView = props => {
 const LoggedInView = props => {
   if (props.currentUser) {
     return (
-      <ul className="nav navbar-nav pull-xs-right">
-
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/editor" className="nav-link">
-            <i className="ion-compose"></i>&nbsp;New Post
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/settings" className="nav-link">
-            <i className="ion-gear-a"></i>&nbsp;Settings
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            to={`/@${props.currentUser.username}`}
-            className="nav-link">
-            {props.currentUser.username}
-          </Link>
-        </li>
-
-      </ul>
+      <Navbar bg="light" variant="light" expand="lg" sticky="top">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/editor" className="nav-link"><i className="ion-compose"></i>&nbsp;New Post</Link>
+            <Link to="/settings" className="nav-link"><i className="ion-gear-a"></i>&nbsp;Settings</Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <Link to={`/@${props.currentUser.username}`} className="nav-link"> {props.currentUser.username}</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 
