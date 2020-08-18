c54f3dd5ab3024f1081b0714d9c8feaeca946bcf
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-18 09:29:17 -0400

    update navbar menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+const bg = "dark"
+const variant = "dark"
+const pullright = "true"
+const navbarStyle = {"border-radius": "0"}
+
 const LoggedOutView = props => {
   if (!props.currentUser) {
     return (
-      <Navbar bg="light" variant="light" expand="lg" sticky="top">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/login" className="nav-link">Sign in</Link>
-            <Link to="/register" className="nav-link">Sign up</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Nav pullright={pullright}>
+        <Nav className="mr-auto">
+          <Link to="/login" className="nav-link">Sign in</Link>
+          <Link to="/register" className="nav-link">Sign up</Link>
+        </Nav>
+      </Nav>
     );
   }
   return null;
@@ -23,24 +24,20 @@ const LoggedOutView = props => {
 const LoggedInView = props => {
   if (props.currentUser) {
     return (
-      <Navbar bg="light" variant="light" expand="lg" sticky="top">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/editor" className="nav-link"><i className="ion-compose"></i>&nbsp;New Post</Link>
-            <Link to="/settings" className="nav-link"><i className="ion-gear-a"></i>&nbsp;Settings</Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-            <Link to={`/@${props.currentUser.username}`} className="nav-link"> {props.currentUser.username}</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Nav pullright={pullright}>
+        <Nav className="mr-auto">
+          <Link to="/editor" className="nav-link"><i className="ion-compose"></i>&nbsp;New Post</Link>
+          <Link to="/settings" className="nav-link"><i className="ion-gear-a"></i>&nbsp;Settings</Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <Link to={`/@${props.currentUser.username}`} className="nav-link"> {props.currentUser.username}</Link>
+        </Nav>
+      </Nav>
     );
   }
 
@@ -50,18 +47,24 @@ const LoggedInView = props => {
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-light">
-        <div className="container">
+      <Navbar bg={bg} variant={variant} expand="sm" sticky="top" style={navbarStyle} >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
 
-          <Link to="/" className="navbar-brand">
-            {this.props.appName.toLowerCase()}
-          </Link>
+          <div className="container">
 
-          <LoggedOutView currentUser={this.props.currentUser} />
+            <Link to="/" className="navbar-brand">
+              {this.props.appName.toLowerCase()}
+            </Link>
 
-          <LoggedInView currentUser={this.props.currentUser} />
-        </div>
-      </nav>
+            <LoggedOutView currentUser={this.props.currentUser} />
+
+            <LoggedInView currentUser={this.props.currentUser} />
+
+          </div>
+
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
