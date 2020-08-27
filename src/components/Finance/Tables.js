4dc03956a1cf1ad754b5f3cce69e2d9261f4732d
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-26 22:45:25 -0400

    add tables for asset

---
+++ src/components/Finance/Tables.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+export const AssetTable = props => {
+    if (props.assets) {
+        return (
+            <Table responsive="sm">
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>type</th>
+                        <th>value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.assets.map(asset => {
+                            return (
+                                <tr>
+                                    <td>{asset.name}</td>
+                                    <td>{asset.typeName}</td>
+                                    <td>{asset.value}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Table>
+        )
+    }
+    return null;
+}
