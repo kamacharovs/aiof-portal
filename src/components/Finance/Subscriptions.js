385a8b9f750280a5ccc66c8e1f1776b3da224c73
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-28 22:23:06 -0400

    update subscriptions

--- src/components/Finance/Subscriptions.js
+++ src/components/Finance/Subscriptions.js
@@ -2,31 +2,37 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { RoundGrayBorderBox, MutedP } from '../../style/common';
 
-export const Subscriptions = props => {
-    if (props.subscriptions) {
-        return (
-            <Container>
-                <Row>
-                    {
-                        props.subscriptions.map(subscription => {
-                            return (
-                                <RoundGrayBorderBox>
-                                    <Row>
-                                        <Col>
-                                            <h2>{subscription.name}</h2>
-                                        </Col>
-                                        <Col>
-                                            <p class="text-muted">$99</p>
-                                        </Col>
-                                    </Row>
-                                    <hr />
-                                </RoundGrayBorderBox>
-                            );
-                        })
-                    }
-                </Row>
-            </Container>
-        )
+class Subscriptions extends React.Component {
+    render() {
+        if (this.props.subscriptions) {
+            return (
+                <Container>
+                    <Row>
+                        {
+                            props.subscriptions.map(subscription => {
+                                return (
+                                    <RoundGrayBorderBox>
+                                        <Row>
+                                            <Col>
+                                                <h2>{subscription.name}</h2>
+                                            </Col>
+                                            <Col>
+                                                <p class="text-muted">$99</p>
+                                            </Col>
+                                        </Row>
+                                        <hr />
+                                        <h3>Name</h3>
+                                        <p class="text-muted">Description</p>
+                                    </RoundGrayBorderBox>
+                                );
+                            })
+                        }
+                    </Row>
+                </Container>
+            )
+        }
+        return null;
     }
-    return null;
 }
+
+export default Subscriptions;
