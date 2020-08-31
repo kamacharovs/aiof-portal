4f88f3141c9d5e748a46db9f7ed9da59cd5995f3
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-30 23:34:43 -0400

    update profile and subscriptions

--- src/components/Finance/Subscriptions.js
+++ src/components/Finance/Subscriptions.js
@@ -1,78 +1,44 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
 import { RoundGrayBorderBox, MutedH2, CoolLink } from '../../style/common';
-
-const mapStateToProps = state => ({
-    ...state.finance,
-    appName: state.common.appName,
-    currentUser: { username: "gkama" }, //state.common.currentUser,
-    subscriptions: state.currentUser.subscriptions,
-});
-
-const mapDispatchToProps = dispatch => ({
-});
 
 class Subscriptions extends React.Component {
     render() {
         if (this.props.subscriptions) {
             return (
                 <React.Fragment>
-                    <Helmet>
-                        <title>{this.props.appName} | Subscriptions</title>
-                    </Helmet>
                     <Container>
-                        <Row>
-                            <Col sm="2">
-                                <Row>
-                                    <Col sm="2">
-                                        <MutedH2>Finance</MutedH2>
-                                        <hr />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col sm="2">
-                                        <nav className="navbar" style={{ padding: "0" }}>
-                                            <ul className="navbar-nav">
-                                                <li className="nav-item">
-                                                    <CoolLink to={`/@${this.props.currentUser.username}/finance/subscriptions`}>Subscriptions</CoolLink>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <CoolLink to="/">Link 2</CoolLink>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <CoolLink to="/">Link 3</CoolLink>
-                                                </li>
-                                            </ul>
-                                        </nav>
-                                    </Col>
-                                </Row>
-                            </Col>
-                            <Col sm="10">
-                                <Row>
-                                    {
-                                        this.props.subscriptions.map(subscription => {
-                                            return (
-                                                <RoundGrayBorderBox>
-                                                    <Row>
-                                                        <Col>
-                                                            <h2>{subscription.name}</h2>
-                                                        </Col>
-                                                        <Col>
-                                                            <p class="text-muted">$99</p>
-                                                        </Col>
-                                                    </Row>
-                                                    <hr />
-                                                    <h3>Name</h3>
-                                                    <p class="text-muted">Description</p>
-                                                </RoundGrayBorderBox>
-                                            );
-                                        })
-                                    }
-                                </Row>
-                            </Col>
-                        </Row>
+                            {
+                                this.props.subscriptions.map(subscription => {
+                                    return (
+                                        <Row>
+                                        <RoundGrayBorderBox>
+                                            <Row>
+                                                <Col>
+                                                    <h5>{subscription.name}</h5>
+                                                </Col>
+                                                <Col>
+                                                    <p className="text-muted">$99</p>
+                                                </Col>
+                                                
+                                            </Row>
+                                            <hr />
+                                            <Row>
+                                                <Col>
+                                                Name
+                                                </Col>
+                                                <Col>
+                                                {subscription.name}
+                                                </Col>
+                                            </Row>
+                                            <Row>
+
+                                            </Row>
+                                        </RoundGrayBorderBox>
+                                        </Row>
+                                    );
+                                })
+                            }
                     </Container>
                 </React.Fragment>
             )
@@ -81,4 +47,4 @@ class Subscriptions extends React.Component {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Subscriptions);
+export default Subscriptions;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import agent from '../agent';
@@ -9,6 +10,7 @@ import {
   PROFILE_PAGE_LOADED,
   PROFILE_PAGE_UNLOADED
 } from '../constants/actionTypes';
+import Subscriptions from './Finance/Subscriptions';
 
 import '../style/tabs.css';
 import { ContainerAiof, CustomHr, Hr50, MutedH2 } from '../style/common';
@@ -130,7 +132,7 @@ const ProfileMain = props => {
           Gross salary
                 </Col>
         <Col xs="6">
-          <b>${grossSalary}</b>
+          <b>{grossSalary}</b>
         </Col>
       </Row>
       <Row>
@@ -156,7 +158,7 @@ const ProfileMain = props => {
           Household income
                 </Col>
         <Col xs="6">
-          <b>${householdIncome}</b>
+          <b>{householdIncome}</b>
         </Col>
       </Row>
       <Row>
@@ -182,7 +184,7 @@ const ProfileMain = props => {
           Retirement contributions pre-tax
                 </Col>
         <Col sm="6">
-          <b>${retirementContributionsPreTax}</b>
+          <b>{retirementContributionsPreTax}</b>
         </Col>
       </Row>
       <Hr50 />
@@ -192,6 +194,7 @@ const ProfileMain = props => {
 
 const mapStateToProps = state => ({
   ...state.profile,
+  appName: state.common.appName,
   currentUser: state.common.currentUser,
   profile: state.profile
 });
@@ -222,7 +225,10 @@ class Profile extends React.Component {
     }
 
     return (
-      <div>
+      <React.Fragment>
+        <Helmet>
+          <title>{this.props.appName} | Profile</title>
+        </Helmet>
         <div style={UserInfo}>
           <Container>
             <Row>
@@ -245,6 +251,7 @@ class Profile extends React.Component {
               <Tab>Profile</Tab>
               <Tab>Finances</Tab>
               <Tab>Notifications</Tab>
+              <Tab>Subscriptions</Tab>
             </TabList>
             <TabPanel>
               <ProfileMain profile={profile} />
@@ -255,9 +262,12 @@ class Profile extends React.Component {
             <TabPanel>
               <h2>Any content 1</h2>
             </TabPanel>
+            <TabPanel>
+              <Subscriptions subscriptions={profile.subscriptions} />
+            </TabPanel>
           </Tabs>
         </ContainerAiof>
-      </div>
+      </React.Fragment>
     );
   }
 }
