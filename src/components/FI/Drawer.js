55580f7d2f45cdd85f6b99f977233884b8bdcc67
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-24 16:58:25 -0400

    update to cleanup

--- src/components/FI/Drawer.js
+++ src/components/FI/Drawer.js
@@ -65,7 +65,7 @@ export const FIDrawer = props => {
 
     return (
         <React.Fragment key={anchor}>
-            <Button onClick={toggleDrawer(true)} style={{color: props.color}}>FI</Button>
+            <Button onClick={toggleDrawer(true)} style={{color: props.color, fontSize: "0.75rem"}}>FI calculators</Button>
             <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(false)}>
                 {list}
             </Drawer>
