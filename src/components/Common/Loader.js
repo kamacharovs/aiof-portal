29d5e33238d248a5c9234fd0dea168eff6e03496
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-20 13:50:08 -0400

    update

--- src/components/Common/Loader.js
+++ src/components/Common/Loader.js
@@ -28,6 +28,7 @@ export const AiofLoader = props => {
 export const AiofGridLoader = props => {
     const inProgress = props.inProgress ? props.inProgress : false;
     const size = props.size ? props.size : defaultSize;
+    const br = props.br ? props.br : true;
     const color = props.color ? props.color : defaultColor;
     const firstGridSize = props.firstGridSize ? props.firstGridSize : defaultFirstGridSize;
     const secondGridSize = props.secondGridSize ? props.secondGridSize : defaultSecondGridSize;
@@ -38,7 +39,7 @@ export const AiofGridLoader = props => {
             </Grid>
             <Grid item xs={secondGridSize}>
                 <div className="sweet-loading">
-                    <br/>
+                    { br ? <br/> : null }
                     <ClipLoader
                         size={size}
                         color={color}
