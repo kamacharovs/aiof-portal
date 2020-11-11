0815fc623fea23bdadfe9264c3817f7800bbb353
Georgi Kamacharov <gkamacharov@gmail.com>
2020-11-11 10:23:39 -0500

    add register load clip

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -22,8 +22,12 @@ export default (state = {}, action) => {
     case REGISTER_PAGE_UNLOADED:
       return {};
     case ASYNC_START:
-      if (action.subtype === LOGIN || action.subtype === REGISTER) {
-        return { ...state, inProgress: true };
+      if (action.subtype === LOGIN 
+        || action.subtype === REGISTER) {
+        return { 
+          ...state,
+          inProgress: true
+        }
       }
       break;
     case UPDATE_FIELD_AUTH:
