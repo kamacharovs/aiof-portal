6d9422104eb4a755a2904b0d693832c041ea8916
Georgi Kamacharov <gkamacharov@gmail.com>
2020-11-23 14:45:18 -0500

    update time to fi to functioning component

--- src/components/FI/TimeToFi.js
+++ src/components/FI/TimeToFi.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import agent from '../../agent';
@@ -35,151 +35,135 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: FI_TIME_TO_FI, payload: agent.Fi.time(timeToFi) })
 });
 
-class TimeToFi extends React.Component {
-  constructor() {
-    super();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '25ch',
+  },
+}));
 
-    this.state = {
-      startingAmount: 800000,
-      monthlyInvestment: 5000,
-      desiredYearsExpensesForFi: 25,
-      desiredAnnualSpending: 100000,
-    };
+const TimeToFi = props => {
+  const classes = useStyles();
+  const [startingAmount, setStartingAmount] = useState(800000);
+  const [monthlyInvestment, setMonthlyInvestment] = useState(5000);
+  const [desiredYearsExpensesForFi, setDesiredYearsExpensesForFi] = useState(25);
+  const [desiredAnnualSpending, setDesiredAnnualSpending] = useState(100000);
 
-    this.classes = makeStyles((theme) => ({
-      root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-      },
-      margin: {
-        margin: theme.spacing(1),
-      },
-      withoutLabel: {
-        marginTop: theme.spacing(3),
-      },
-      textField: {
-        width: '25ch',
-      },
-    }));
+  const onSubmitForm = ev => {
+    ev.preventDefault();
 
-    this.updateState = field => ev => {
-      const state = this.state;
-      const newState = Object.assign({}, state, { [field]: ev.target.value });
-      this.setState(newState);
-    };
+    let timeToFi = {};
+    timeToFi.startingAmount = startingAmount ? Number(startingAmount) : null;
+    timeToFi.monthlyInvestment = monthlyInvestment ? Number(monthlyInvestment) : null;
+    timeToFi.desiredYearsExpensesForFi = desiredYearsExpensesForFi ? Number(desiredYearsExpensesForFi) : null;
+    timeToFi.desiredAnnualSpending = desiredAnnualSpending ? Number(desiredAnnualSpending) : null;
 
-    this.submitForm = ev => {
-      ev.preventDefault();
+    props.onSubmit(timeToFi);
+  };
 
-      const timeToFi = Object.assign({}, this.state);
+  useEffect(() => {
+    props.onLoad();
 
-      timeToFi.startingAmount = timeToFi.startingAmount ? Number(timeToFi.startingAmount) : null;
-      timeToFi.monthlyInvestment = timeToFi.monthlyInvestment ? Number(timeToFi.monthlyInvestment) : null;
-      timeToFi.desiredYearsExpensesForFi = timeToFi.desiredYearsExpensesForFi ? Number(timeToFi.desiredYearsExpensesForFi) : null;
-      timeToFi.desiredAnnualSpending = timeToFi.desiredAnnualSpending ? Number(timeToFi.desiredAnnualSpending) : null;
-
-      this.props.onSubmit(timeToFi);
-    };
-  }
-
-  componentDidMount() {
-    this.props.onLoad();
-
-    if (this.props.time) {
-      this.setState({
-        startingAmount: this.props.time.startingAmount,
-        monthlyInvestment: this.props.time.monthlyInvestment,
-        desiredYearsExpensesForFi: this.props.time.desiredYearsExpensesForFi,
-        desiredAnnualSpending: this.props.time.desiredAnnualSpending,
-      });
+    if (props.time) {
+      setStartingAmount(props.time.startingAmount);
+      setMonthlyInvestment(props.time.monthlyInvestment);
+      setDesiredYearsExpensesForFi(props.time.desiredYearsExpensesForFi);
+      setDesiredAnnualSpending(props.time.desiredAnnualSpending);
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <React.Fragment>
-        <Helmet>
-          <title>{this.props.appName} | Time to FI</title>
-        </Helmet>
-        <Container maxWidth="sm">
-          <AiofPaper elevation={3}>
-            <form className={this.classes.root} noValidate autoComplete="off" onSubmit={this.submitForm}>
-              <Grid container spacing={3}>
-                <Grid item xs={6}>
-                  <div className={this.classes.margin}>
-                    <Grid container spacing={1} alignItems="flex-end">
-                      <Grid item>
-                        <AttachMoneyIcon />
-                      </Grid>
-                      <Grid item>
-                        <TextField id="input-with-icon-grid" label="Starting amount"
-                          value={this.state.startingAmount}
-                          onChange={this.updateState('startingAmount')} />
-                      </Grid>
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{props.appName} | Time to FI</title>
+      </Helmet>
+      <Container maxWidth="sm">
+        <AiofPaper elevation={3}>
+          <form className={classes.root} noValidate autoComplete="off" onSubmit={onSubmitForm}>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <div className={classes.margin}>
+                  <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                      <AttachMoneyIcon />
                     </Grid>
-                  </div>
-                </Grid>
-
-                <Grid item xs={6}>
-                  <div className={this.classes.margin}>
-                    <Grid container spacing={1} alignItems="flex-end">
-                      <Grid item>
-                        <AttachMoneyIcon />
-                      </Grid>
-                      <Grid item>
-                        <TextField id="input-with-icon-grid" label="Monthly investment"
-                          value={this.state.monthlyInvestment}
-                          onChange={this.updateState('monthlyInvestment')} />
-                      </Grid>
+                    <Grid item>
+                      <TextField id="input-with-icon-grid" label="Starting amount"
+                        value={startingAmount}
+                        onChange={e => setStartingAmount(e.target.value)} />
                     </Grid>
-                  </div>
-                </Grid>
-
-                <Grid item xs={6}>
-                  <div className={this.classes.margin}>
-                    <Grid container spacing={1} alignItems="flex-end">
-                      <Grid item>
-                        <ArrowUpwardIcon />
-                      </Grid>
-                      <Grid item>
-                        <TextField id="input-with-icon-grid" label="Years expenses"
-                          value={this.state.desiredYearsExpensesForFi}
-                          onChange={this.updateState('desiredYearsExpensesForFi')} />
-                      </Grid>
-                    </Grid>
-                  </div>
-                </Grid>
-
-                <Grid item xs={6}>
-                  <div className={this.classes.margin}>
-                    <Grid container spacing={1} alignItems="flex-end">
-                      <Grid item>
-                        <AttachMoneyIcon />
-                      </Grid>
-                      <Grid item>
-                        <TextField id="input-with-icon-grid" label="Annual spending"
-                          value={this.state.desiredAnnualSpending}
-                          onChange={this.updateState('desiredAnnualSpending')} />
-                      </Grid>
-                    </Grid>
-                  </div>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="primary" className={this.classes.button} >
-                    Calculate
-                </Button>
-                </Grid>
+                  </Grid>
+                </div>
               </Grid>
-            </form>
-          </AiofPaper>
 
-          <TimeToFiResults time={this.props.time} inProgress={this.props.inProgress} />
+              <Grid item xs={6}>
+                <div className={classes.margin}>
+                  <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                      <AttachMoneyIcon />
+                    </Grid>
+                    <Grid item>
+                      <TextField id="input-with-icon-grid" label="Monthly investment"
+                        value={monthlyInvestment}
+                        onChange={e => setMonthlyInvestment(e.target.value)} />
+                    </Grid>
+                  </Grid>
+                </div>
+              </Grid>
 
-        </Container>
-      </React.Fragment>
-    );
-  }
+              <Grid item xs={6}>
+                <div className={classes.margin}>
+                  <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                      <ArrowUpwardIcon />
+                    </Grid>
+                    <Grid item>
+                      <TextField id="input-with-icon-grid" label="Years expenses"
+                        value={desiredYearsExpensesForFi}
+                        onChange={e => setDesiredYearsExpensesForFi(e.target.value)} />
+                    </Grid>
+                  </Grid>
+                </div>
+              </Grid>
+
+              <Grid item xs={6}>
+                <div className={classes.margin}>
+                  <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                      <AttachMoneyIcon />
+                    </Grid>
+                    <Grid item>
+                      <TextField id="input-with-icon-grid" label="Annual spending"
+                        value={desiredAnnualSpending}
+                        onChange={e => setDesiredAnnualSpending(e.target.value)} />
+                    </Grid>
+                  </Grid>
+                </div>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" color="primary" className={classes.button} >
+                  Calculate
+              </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </AiofPaper>
+
+        <TimeToFiResults time={props.time} inProgress={props.inProgress} />
+
+      </Container>
+    </React.Fragment>
+  );
 }
 
 const TimeToFiResults = props => {
