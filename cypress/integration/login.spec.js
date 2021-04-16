a6853d49d47c6c9c0ec6c6400b9728f3ea872ab7
Georgi Kamacharov <gkamacharov@gmail.com>
2021-04-15 22:47:40 -0400

    update ids

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,21 +7,21 @@ describe('Login', () => {
     var password = Cypress.env('password')
 
     it('login is successful', () => {
-        cy.get('[id="login-email"]')
+        cy.get('#login-email')
             .type(email)
             .should('have.value' , email)
 
-        cy.get('[id="login-password"]')
+        cy.get('#login-password')
             .type(password)
             .should('have.value' , password)
 
-        cy.get('[id="login-button"]')
+        cy.get('#login-button')
             .click()
             .should('be.disabled')
     })
 
     it('navigate to registration is successful', () => {
-        cy.get('[id="login-link-register"]')
+        cy.get('#login-link-register')
             .click()
 
         cy.location().should((location) => {
@@ -30,7 +30,7 @@ describe('Login', () => {
     })
 
     it('navigate to terms and conditions is successful', () => {
-        cy.get('[id="login-link-terms-and-conditions"]')
+        cy.get('#login-link-terms-and-conditions')
             .click()
 
         cy.location().should((location) => {
@@ -39,7 +39,7 @@ describe('Login', () => {
     })
 
     it('navigate to privacy policy is successful', () => {
-        cy.get('[id="login-link-privacy-policy"]')
+        cy.get("#login-link-privacy-policy")
             .click()
 
         cy.location().should((location) => {
