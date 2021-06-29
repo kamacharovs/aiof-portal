01c6614008dc557989af59535aea022f081b70e2
Georgi Kamacharov <gkamacharov@gmail.com>
2021-06-29 09:20:53 -0400

    add common inputs text fields

---
+++ src/components/Common/Inputs.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+export const TextFieldInputAdornment = props => {
+    const label = props.label;
+    const value = props.value;
+    const onChange = props.onChange;
+
+    return <TextFieldBaseInputAdornment
+                label={label}
+                value={value}
+                onChange={onChange} />
+}
+
+export const TextFieldMoneyInputAdornment = props => {
+    const label = props.label;
+    const value = props.value;
+    const onChange = props.onChange;
+
+    return <TextFieldBaseInputAdornment
+                label={label}
+                value={value}
+                onChange={onChange}
+                adornmentValue={"$"} />
+}
+
+const TextFieldBaseInputAdornment = props => {
+    const label = props.label;
+    const value = props.value;
+    const onChange = props.onChange;
+    const adornmentPosition = props.adornmentPosition || "start";
+    const adornmentValue = props.adornmentValue || null;
+
+    return (
+        <TextField label={label}
+            value={value}
+            onChange={onChange}
+            InputProps={{
+                startAdornment: <InputAdornment position={adornmentPosition}>{adornmentValue}</InputAdornment>
+            }} />
+    );
+}
